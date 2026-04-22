04e556bb95b6c7678629836ff2d86a869b5507ed
James Kalisz <james.kalisz@tdsb.on.ca>
2026-04-22 10:23:12 -0400

    update files

--- src/main/java/MyUtilityClass.java
+++ src/main/java/MyUtilityClass.java
@@ -1,11 +1,39 @@
 public final class MyUtilityClass {
-
-  final static MAXIMUM = 3000;
   
+  //Constants
+  final static int MAXIMUM = 3000;
+
+  //Private constructor so an object can't be created
   private MyUtilityClass() {
   }
-
+  
+  
+  //FUNCTION - does not require an object to use.
   public static int returnZero() {
     return 0;
+  }
+
+  //overloading functions
+  public static int returnZero(int num) {
+    return 0;
+  }
+
+  public static double returnZero(double num) {
+    return 0.0;
+  }
+
+  //overloading methods and constructors work the same way
+  public static int min(int num1, int num2) {
+    if (num1 < num2) {
+      return num1;
+    }
+    return num2;
+  }
+  
+  public static double min(double num1, double num2) {
+    if (num1 < num2) {
+      return num1;
+    }
+    return num2;
   }
 }
--- src/main/java/Notes.java
+++ src/main/java/Notes.java
@@ -1,6 +1,12 @@
 
 class Notes {
 	public static void main(String[] args) {
-
+		System.out.println(MyUtilityClass.returnZero());
+		System.out.println(MyUtilityClass.returnZero(3.0));
+		System.out.println(MyUtilityClass.returnZero(3));
+		System.out.println(MyUtilityClass.min(2, 4));
+		System.out.println(MyUtilityClass.min(2.5, 4.3));
+		System.out.println(MyUtilityClass.min(2.5, 4));
+		System.out.println(MyUtilityClass.min(2, 4.5));
 	}
 }
